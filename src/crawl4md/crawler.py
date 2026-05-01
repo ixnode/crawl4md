2272fb3b9f7d9c1b48ab74f55c8fde0bfc2dd26d
Björn Hempel <bjoern@hempel.li>
2026-05-01 23:58:34 +0200

    Fix crawler settings

--- src/crawl4md/crawler.py
+++ src/crawl4md/crawler.py
@@ -1,6 +1,6 @@
 import logging
 
-from crawl4ai import AsyncWebCrawler, CrawlerRunConfig
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, BrowserConfig
 from crawl4ai.content_filter_strategy import PruningContentFilter
 from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 
@@ -11,20 +11,18 @@ logging.getLogger("crawl4ai").setLevel(logging.ERROR)
 
 
 async def fetch_markdown(url: str, parse_type: ParseType = "markdown") -> str:
-    markdown_generator = None
-
     if parse_type == "markdown-fit":
         markdown_generator = DefaultMarkdownGenerator(
             content_filter=PruningContentFilter(
-                threshold=0.4,
-                threshold_type="fixed",
-                min_word_threshold=5,
-            )
+                threshold=0.5
+            ),
+            options={"ignore_links": False},
         )
-
-    config = CrawlerRunConfig(
-        markdown_generator=markdown_generator,
-    )
+        config = CrawlerRunConfig(
+            markdown_generator=markdown_generator,
+        )
+    else:
+        config = CrawlerRunConfig()
 
     async with AsyncWebCrawler() as crawler:
         result = await crawler.arun(
