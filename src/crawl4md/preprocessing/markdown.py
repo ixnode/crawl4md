520c4de376080d84ec3558e7be94df4eb809a58d
Björn Hempel <bjoern@hempel.li>
2026-05-02 18:28:04 +0200

    Add file header

--- src/crawl4md/preprocessing/markdown.py
+++ src/crawl4md/preprocessing/markdown.py
@@ -1,3 +1,14 @@
+# This file is part of the https://github.com/ixnode/crawl4md project.
+#
+# (c) 2026 Björn Hempel <bjoern@hempel.li>
+#
+# For the full copyright and license information, please view the LICENSE.md
+# file that was distributed with this source code.
+#
+# @author: Björn Hempel <bjoern@hempel.li>
+# @version: 1.0.0 (2026-05-02)
+# @since 1.0.0 (2026-05-02) First version
+
 from .rules.rule_base import RuleBase
 from .rules.ensure_h1 import RuleEnsureH1
 from .rules.normalize_whitespace import RuleNormalizeWhitespace
