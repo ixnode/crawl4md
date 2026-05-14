b5a48286f2d2f350256304126648715835b9fad1
Björn Hempel <bjoern.hempel@ea-energie.de>
2026-05-14 23:35:21 +0200

    Run remove_lines after remove_links in markdown pipeline

--- src/crawl4md/convert/preprocessing/markdown.py
+++ src/crawl4md/convert/preprocessing/markdown.py
@@ -37,14 +37,14 @@ class MarkdownPreprocessing:
         if config.remove_blocks:
             self.rules.append(RuleRemoveBlocks(config))
 
-        if config.remove_lines:
-            self.rules.append(RuleRemoveLines(config))
-
         if config.remove_images:
             self.rules.append(RuleRemoveImages(config))
 
         if config.remove_links:
             self.rules.append(RuleRemoveLinks(config))
+
+        if config.remove_lines:
+            self.rules.append(RuleRemoveLines(config))
 
         if config.ensure_h1:
             self.rules.append(RuleEnsureH1(config))
