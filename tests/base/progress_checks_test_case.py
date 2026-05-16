6674b630a5dcb0f5c28fdbc33a73645463d39e7f
Björn Hempel <bjoern@hempel.li>
2026-05-17 00:11:45 +0200

    Type progress check classes via Protocol with CHECK_METHODS to satisfy IDE

--- tests/base/progress_checks_test_case.py
+++ tests/base/progress_checks_test_case.py
@@ -1,15 +1,31 @@
+# This file is part of the https://github.com/ixnode/crawl4md project.
+#
+# (c) 2026 Björn Hempel <bjoern@hempel.li>
+#
+# For the full copyright and license information, please view the LICENSE.md
+# file that was distributed with this source code.
+#
+# @author: Björn Hempel <bjoern@hempel.li>
+# @version: 1.0.0 (2026-05-16)
+# @since 1.0.0 (2026-05-16) First version
+
 import unittest
+
 from collections.abc import Callable
-from typing import Any
+from typing import Protocol
 
 from tests.support.progress import run_progress_cases
+
+
+class ProgressChecksProtocol(Protocol):
+    CHECK_METHODS: tuple[str, ...]
 
 
 class ProgressChecksTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.maxDiff = None
 
-    def run_progress_check_methods(self, checks_class: type[Any]) -> None:
+    def run_progress_check_methods(self, checks_class: type[ProgressChecksProtocol]) -> None:
         checks: list[tuple[str, Callable[[], None]]] = [
             (
                 f"test_{method_name}",
